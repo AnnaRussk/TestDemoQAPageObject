7f76d2d74b1f110740f2906baec7ee452f67a418
@russkikh_anna <anna.russkih13@gmail.com>
2025-03-17 12:42:07 +0700

    Актуализированы импорты

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,10 +3,8 @@ import static com.codeborne.selenide.Selenide.*;
 
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
     protected static final String url = "https://demoqa.com/";
